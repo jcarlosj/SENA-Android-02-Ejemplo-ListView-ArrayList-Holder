b4df5d11b7d62bca97b2b8c50e317827b5c6a86f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2015-10-08 21:46:00 -0500

    Agrega funcionalidad al "MainActivity"
     - Agrega el método "init()" donde se accede al componente "ListView"
       del "Activity", Se agregan datos a objetos de tipo "Animal" a un
       "ArrayList" que es asociado a al "AnimalesAdapter" a travé de su
       instancia que luego es asociada al "ListView".
     - Agrega todas las imagenes usadas en el "ListView" y que son pasadas
       como recurso al método "Animal"

--- app/src/main/java/co/jce/sena/listviewpersonalizadaarrayadapter/MainActivity.java
+++ app/src/main/java/co/jce/sena/listviewpersonalizadaarrayadapter/MainActivity.java
@@ -4,13 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    //-> Atributos (Componentes)
+    private ListView lvAnimales;
+
+    //-> Atributos (Colecciones)
+    private ArrayList< Animal > alAnimales;
+
+    //-> Atributos (Especiales)
+    private AnimalesAdapter adaptadorAnimales;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
     }
 
     @Override
@@ -34,4 +47,41 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void agregarAnimales() {
+
+        alAnimales = new ArrayList< Animal >();
+
+        alAnimales .add( new Animal( R .drawable .ic_horse, "Caballo", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_snail, "Caracol", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_rabbit, "Conejo", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_elephant, "Elefante", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_cat, "Gato", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_lobster, "Langosta", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_lion, "León", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_leopard, "Leopardo", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_ladybug, "Mariquita", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_bird, "Pájaro", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_duck, "Pato", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_dog, "Perro", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_fish, "Pez", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_bull, "Toro", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_turtle, "Tortuga", "Descripción" ) );
+        alAnimales .add( new Animal( R .drawable .ic_cow, "Vaca", "Descripción" ) );
+    }
+
+    private void init() {
+        //-> Accedemos a los componentes del "Activity".
+        lvAnimales = ( ListView ) findViewById( R .id .lvAnimales );        //: ListView
+
+        agregarAnimales();
+
+        //-> Instanciamos el Adaptador.
+        //   Asociamos el "Adapter" al "ArrayList".
+        adaptadorAnimales = new AnimalesAdapter( this, alAnimales );
+
+        //-> Asociamos el "Adapter" con el "ListView".
+        lvAnimales .setAdapter( adaptadorAnimales );
+    }
+
 }
